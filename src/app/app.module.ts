2477a92630257454ae3efdd0446d6c5ef94137e2
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2021-01-29 13:50:06 +0530

    add angular material with materialmodule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,51 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterComponent } from './components/filter/filter.component';
+import { AdvertisementCardComponent } from './components/advertisement-card/advertisement-card.component';
+import { AdvertisementFeedComponent } from './components/advertisement-feed/advertisement-feed.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
+import { PromotionsComponent } from './components/promotions/promotions.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AdvertisementFormComponent } from './components/advertisement-form/advertisement-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { AdvertisementViewComponent } from './components/advertisement-view/advertisement-view.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    AdvertisementCardComponent,
+    AdvertisementFeedComponent,
+    SearchResultsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MainComponent,
+    PromotionsComponent,
+    SocialMediaComponent,
+    UserProfileComponent,
+    AdvertisementFormComponent,
+    LoginComponent,
+    AboutUsComponent,
+    AdvertisementViewComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatToolbarModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
